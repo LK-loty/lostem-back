73861981e2d74049f167442037a80caa24b971c5
kim <yahyunjin5@gmail.com>
2023-08-04 19:05:04 +0900

    Initial commit

---
+++ src/main/java/loty/lostem/LostemApplication.java
@@ -0,0 +1,13 @@
+package loty.lostem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostemApplication.class, args);
+	}
+
+}
