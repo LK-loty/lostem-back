f61d14295e319e38472795ba6df42f133f22028f
kim <yahyunjin5@gmail.com>
2023-09-14 17:12:04 +0900

    add postToDTO method to Post Service, create method comment - change return result post entity to postDTO - consideration comment : entity parameter need to change DTO

--- src/main/java/loty/lostem/service/PostService.java
+++ src/main/java/loty/lostem/service/PostService.java
@@ -16,25 +16,31 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Transactional
-    public PostDTO createPost(PostDTO getDTO, User user) {
-        Post created = Post.createPost(getDTO, user);
+    public PostDTO createPost(PostDTO getDTO, User user) {  // userDTO를 받아서 user 엔티티로 변환하고 전달할 것인가
+        Post created = Post.createPost(getDTO, user);  // 변환하여 전달하려면 user 쪽의 변환 메소드 사용??
         postRepository.save(created);
-        PostDTO createdDTO = PostDTO.builder()
-                .post_id(created.getPost_id())
-                .title(getDTO.getTitle())
-                .image(getDTO.getImage())
-                .type(getDTO.getType())
-                .period(getDTO.getPeriod())
-                .place(getDTO.getPlace())
-                .item(getDTO.getItem())
-                .explain(getDTO.getExplain())
-                .state(getDTO.getState())
-                .report(getDTO.getReport())
-                .time(getDTO.getTime())
-                .category(getDTO.getCategory())
-                .storage(getDTO.getStorage())
-                .build();
+        PostDTO createdDTO = postToDTO(created);
         return createdDTO;
     }
 
+
+
+
+    public PostDTO postToDTO(Post post) {
+        return PostDTO.builder()
+                .post_id(post.getPost_id())
+                .title(post.getTitle())
+                .image(post.getImage())
+                .type(post.getType())
+                .period(post.getPeriod())
+                .place(post.getPlace())
+                .item(post.getItem())
+                .explain(post.getExplain())
+                .state(post.getStorage())
+                .report(post.getReport())
+                .time(post.getTime())
+                .category(post.getCategory())
+                .storage(post.getStorage())
+                .build();
+    }
 }
