55ba4089dd0587328af6185dee07c3d56a7e22ca
kim <yahyunjin5@gmail.com>
2023-10-22 17:25:46 +0900

    add security, auditing annotation - main Application

--- src/main/java/loty/lostem/LostemApplication.java
+++ src/main/java/loty/lostem/LostemApplication.java
@@ -2,8 +2,12 @@ package loty.lostem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableJpaAuditing // auditing, baseTimeEntity
 public class LostemApplication {
 
 	public static void main(String[] args) {
