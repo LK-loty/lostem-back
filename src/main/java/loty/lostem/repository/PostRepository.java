0f3cde6cfba758c1d65d69c63e471a7b34aa4a64
kim <yahyunjin5@gmail.com>
2023-11-17 16:17:58 +0900

    add Query Methods in user and post - dropped post file

--- src/main/java/loty/lostem/repository/PostRepository.java
+++ src/main/java/loty/lostem/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package loty.lostem.repository;
 
 import loty.lostem.entity.Post;
+import loty.lostem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    //User findByUser_UserId(Long userId);
+    List<Post> findByUser_UserId(Long userId);
+
 }
