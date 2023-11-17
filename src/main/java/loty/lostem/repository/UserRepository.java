139fb7969850623a47668e614f33db1bdd315f13
kim <yahyunjin5@gmail.com>
2023-11-17 15:26:20 +0900

    add Query Methods in user and post - userRepository   : name >> find user, exist user   : name&password >> authorization   : authorities >> authorization check - postRepository   : userId >> find user

--- src/main/java/loty/lostem/repository/UserRepository.java
+++ src/main/java/loty/lostem/repository/UserRepository.java
@@ -1,7 +1,20 @@
 package loty.lostem.repository;
 
 import loty.lostem.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+
+    boolean existsByUsername(String username);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
 }
