e14f83f5098e5f073e7c49f4c6cee7d81c405698
kim <yahyunjin5@gmail.com>
2023-12-01 17:38:20 +0900

    edit SecurityConfig - modify url because of reading user to permit all

--- src/main/java/loty/lostem/config/SecurityConfig.java
+++ src/main/java/loty/lostem/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(
-                                "/api/posts/read/**", "/api/search", "/api/users/read", "/api/users/signup","/api/login"
+                                "/api/posts/read/**", "/api/search", "/api/users/**", "/api/users/signup","/api/login"
                         )
                         .permitAll()
 
