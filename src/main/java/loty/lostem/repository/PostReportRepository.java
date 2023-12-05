39020fec0273d7afb7d7ec4ec0a6891204143d57
kim <yahyunjin5@gmail.com>
2023-12-05 17:47:55 +0900

    add PostReport repository - add Query method by PostId

--- src/main/java/loty/lostem/repository/PostReportRepository.java
+++ src/main/java/loty/lostem/repository/PostReportRepository.java
@@ -3,5 +3,8 @@ package loty.lostem.repository;
 import loty.lostem.entity.PostReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostReportRepository extends JpaRepository<PostReport, Long> {
+    Optional<PostReport> findByPost_PostId(Long postId);
 }
