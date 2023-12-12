e049b40246a6ea12cbe50a46d61cf46531328ea1
kim <yahyunjin5@gmail.com>
2023-12-12 17:35:39 +0900

    add WebSocketConfig - initial websocket configuration - before apply stomp

---
+++ src/main/java/loty/lostem/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package loty.lostem.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");  // WebSocket 핸들러를 "/ws/chat" 엔드포인트에 매핑, 모든 ip에서 접속 가능
+    }
+
+}
