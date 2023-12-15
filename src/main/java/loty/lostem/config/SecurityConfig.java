cfcedc594c9536a9ba95a19b4f8824a5f96c255c
kim <yahyunjin5@gmail.com>
2023-12-15 15:55:18 +0900

    edit SecurityConfig for chat - add chatService url for authentication

--- src/main/java/loty/lostem/config/SecurityConfig.java
+++ src/main/java/loty/lostem/config/SecurityConfig.java
@@ -69,6 +69,11 @@ public class SecurityConfig {
                         .permitAll()
 
                         .requestMatchers(
+                                "/api/websocket"
+                        )
+                        .authenticated()
+
+                        .requestMatchers(
                                 "/admin/**"
                         )
                         .hasRole(UserRole.ADMIN.name())
