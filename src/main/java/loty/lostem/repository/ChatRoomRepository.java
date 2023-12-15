10413a6345453ed50b9dd0486dc23c4e8d7ecff5
kim <yahyunjin5@gmail.com>
2023-12-15 15:54:13 +0900

    add query method to ChatRoomRepository - find chatRoom by HostUserId

--- src/main/java/loty/lostem/repository/ChatRoomRepository.java
+++ src/main/java/loty/lostem/repository/ChatRoomRepository.java
@@ -3,5 +3,8 @@ package loty.lostem.repository;
 import loty.lostem.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    List<ChatRoom> findByHostUser_HostUserId(Long hostUserId);
 }
