09da1cedee7bc84f475e114eeb02c8edbeb33d28
kim <yahyunjin5@gmail.com>
2024-02-05 16:00:37 +0900

    test for websocket handshaking - fix corsConfig

--- src/main/java/loty/lostem/config/CorsConfig.java
+++ src/main/java/loty/lostem/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
+        //config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
