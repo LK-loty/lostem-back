1c531c72aff10777722e583894599ace21836da5
kim <yahyunjin5@gmail.com>
2024-02-14 20:18:18 +0900

    update create Room and Message to chatService - when a user sends a message for the first time, chatRoom should be created. sending form is same, url is separated by /chat/enter and /chat/message - create Room by postId at first, and save message
    - message sender's user ID is token userId

--- src/main/java/loty/lostem/controller/MessageController.java
+++ src/main/java/loty/lostem/controller/MessageController.java
@@ -3,6 +3,7 @@ package loty.lostem.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import loty.lostem.dto.ChatMessageDTO;
+import loty.lostem.dto.ChatRoomDTO;
 import loty.lostem.jwt.TokenProvider;
 import loty.lostem.service.ChatService;
 import org.springframework.http.ResponseEntity;
@@ -21,76 +22,38 @@ public class MessageController {
     private final TokenProvider tokenProvider;
     private final ChatService chatService;
 
+    @MessageMapping("/chat/enter") // 처음 입장 시 메세지 출력
+    public void enterRoom(@Payload ChatMessageDTO messageDTO, @Header("Authorization") String token) {
+        Long userId = tokenProvider.getUserId(token);
+        if (messageDTO.getRoomId() == null || messageDTO.equals("null")) {  // ChatMessageDTO.MessageType.ENTER.equals(messageDTO.getType())
+            ChatRoomDTO roomDTO = chatService.createRoom(messageDTO, userId);
+            simpMessageSendingOperations.convertAndSend("/sub/chat/room/" + roomDTO.getRoomId(),messageDTO.getSender()+"님이 입장하셨습니다. " + messageDTO.getMessage());
+        }
+        log.info("채팅방이 생성되었습니다");
+    }
+
     // /pub/chat/message 로 요청이 들어오면 해당 메서드 실행 후 /sub/chat/room/id 구독하는 사용자들에게 요청할 때 같이온 chatRequest 실행
     @MessageMapping("/chat/message") // setApplicationDestinationPrefixes()를 통해 prefix "/pub"으로 설정 했으므로, 경로가 한번 더 수정되어 “/pub/chat/message”로 바뀜
-    public void message(@Payload ChatMessageDTO message, @Header("token") String token) { // (@Payload ChatRequest chatRequest, SimpMessageHeaderAccessor headerAccessor)
+    public void message(@Payload ChatMessageDTO messageDTO, @Header("Authorization") String token) { // (@Payload ChatRequest chatRequest, SimpMessageHeaderAccessor headerAccessor)
         // 토큰 검사하면서 유저 추출? 유효성 검사 로직 정리 필요
-        log.info("채팅");
+        tokenProvider.validateToken(token);
+        log.info("유효한 토큰이므로 채팅을 계속합니다.");
         Long userId = tokenProvider.getUserId(token);
-        if (ChatMessageDTO.MessageType.TALK.equals(message.getType())) {
-            chatService.createMessage(message);
-            simpMessageSendingOperations.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
-        }
 
+        chatService.createMessage(messageDTO, userId);
+        simpMessageSendingOperations.convertAndSend("/sub/chat/room/" + messageDTO.getRoomId(), messageDTO);
         /*// Redis를 통해 메시지를 발행하여 채팅방의 특정 토픽에 메시지 전송
         redisTemplate.convertAndSend("/topic/public", chatMessage);*/
     }
 
-    @MessageMapping("/chat/{roomNo}")
-    @SendTo("/sub/chat/{roomNo}") //
-    public void broadcasting(final ChatMessageDTO messageDTO, @DestinationVariable(value = "roomNo") final String chatRoomNo) {
-        chatService.createMessage(messageDTO);
-    }
-
-    /*@MessageMapping("/chat/{id}")
-    public void sendMessage(@Payload MessageDTO messageDTO, @DestinationVariable Integer id){
-        this.simpMessagingTemplate.convertAndSend("/queue/addChatToClient/"+id,messageDTO);
+    /*@MessageMapping("/chat/{roomId}")
+    @SendTo("/sub/chat/{roomId}") //
+    public void broadcasting(final ChatMessageDTO messageDTO, @DestinationVariable(value = "roomId") final Long roomId) {
+        //////this.simpMessagingTemplate.convertAndSend("/queue/addChatToClient/"+id,messageDTO);
+        chatService.createMessage(roomId, messageDTO, );
     }*/
 
-    @MessageMapping("/chat/enter") // 처음 입장 시 메세지 출력
-    public void enter(@Payload ChatMessageDTO message, @Header("token") String token) {
-        Long userId = tokenProvider.getUserId(token);
-        if (ChatMessageDTO.MessageType.ENTER.equals(message.getType())) {
-            chatService.createMessage(message);
-            simpMessageSendingOperations.convertAndSend("/sub/chat/room/"+message.getRoomId(),message.getSender()+"님이 입장하셨습니다");
-        }
-        System.out.println("채팅방 생성");
-    }
-    @GetMapping("/chat/get")
-    public ResponseEntity<List<ChatMessageDTO>> getMessages(@PathVariable Long roomId) {
-        // 채팅방에서의 모든 메시지를 가져오는 요청을 서비스에 전달하여 처리
-        List<ChatMessageDTO> messages = chatService.getAllMessages(roomId);
-        return ResponseEntity.ok(messages);
-    }
-
-    //
-    /*@MessageMapping("/chat/enterUser")
-    public void enterUser(@Payload ChatDTO chat, SimpMessageHeaderAccessor headerAccessor) {
-
-        // 채팅방 유저+1
-        repository.plusUserCnt(chat.getRoomId());
-
-        // 채팅방에 유저 추가 및 UserUUID 반환
-        String userUUID = repository.addUser(chat.getRoomId(), chat.getSender());
-
-        // 반환 결과를 socket session 에 userUUID 로 저장
-        headerAccessor.getSessionAttributes().put("userUUID", userUUID);
-        headerAccessor.getSessionAttributes().put("roomId", chat.getRoomId());
-
-        chat.setMessage(chat.getSender() + " 님 입장!!");
-        template.convertAndSend("/sub/chat/room/" + chat.getRoomId(), chat);
-
-    }
-
-    // 해당 유저
-    @MessageMapping("/chat/sendMessage")
-    public void sendMessage(@Payload ChatDTO chat) {
-        log.info("CHAT {}", chat);
-        chat.setMessage(chat.getMessage());
-        template.convertAndSend("/sub/chat/room/" + chat.getRoomId(), chat);
-
-    }
-
+    /*
     // 유저 퇴장 시에는 EventListener 을 통해서 유저 퇴장을 확인
     @EventListener
     public void webSocketDisconnectListener(SessionDisconnectEvent event) {
@@ -123,25 +86,5 @@ public class MessageController {
 
             template.convertAndSend("/sub/chat/room/" + roomId, chat);
         }
-    }
-
-    // 채팅에 참여한 유저 리스트 반환
-    @GetMapping("/chat/userlist")
-    @ResponseBody
-    public ArrayList<String> userList(String roomId) {
-
-        return repository.getUserList(roomId);
-    }
-
-    // 채팅에 참여한 유저 닉네임 중복 확인
-    @GetMapping("/chat/duplicateName")
-    @ResponseBody
-    public String isDuplicateName(@RequestParam("roomId") String roomId, @RequestParam("username") String username) {
-
-        // 유저 이름 확인
-        String userName = repository.isDuplicateName(roomId, username);
-        log.info("동작확인 {}", userName);
-
-        return userName;
     }*/
 }
--- src/main/java/loty/lostem/service/ChatService.java
+++ src/main/java/loty/lostem/service/ChatService.java
@@ -6,13 +6,14 @@ import loty.lostem.dto.ChatMessageDTO;
 import loty.lostem.dto.ChatRoomDTO;
 import loty.lostem.entity.ChatMessage;
 import loty.lostem.entity.ChatRoom;
+import loty.lostem.entity.Post;
 import loty.lostem.entity.User;
 import loty.lostem.repository.ChatMessageRepository;
 import loty.lostem.repository.ChatRoomRepository;
+import loty.lostem.repository.PostRepository;
 import loty.lostem.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,26 +24,45 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     // 채팅방
     @Transactional
-    public ChatRoomDTO createRoom(ChatRoomDTO chatRoomDTO) {
-        User host = userRepository.findById(chatRoomDTO.getHostUserId())
-                .orElseThrow(() -> new IllegalArgumentException("No host"));
-        User guest = userRepository.findById(chatRoomDTO.getHostUserId())
+    public ChatRoomDTO createRoom(ChatMessageDTO messageDTO, Long userId) {
+        /*if (chatRoomDTO.getPostId()) {
+            // 유저가 이미 그 상대와 채팅방 만들었다면 그 채팅방으로 이동
+        }*/
+        User guest = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("No guest"));
-        ChatRoom chatRoom = ChatRoom.createChatRoom(chatRoomDTO, host, guest);
+        Post post = postRepository.findById(messageDTO.getPostId())
+                .orElseThrow(() -> new IllegalArgumentException("No post"));
+        User host = userRepository.findById(post.getUser().getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("No host"));
+
+        ChatRoom chatRoom = ChatRoom.createChatRoom(host, guest, messageDTO.getPostId());
         chatRoomRepository.save(chatRoom); // user 마다 채팅방 리스트화하면 저장할 때 힘듦+검색은 비교적 편함? 지금 로직은 생성할 때 바로 저장+검색은 오래 걸릴수도?
+
+        ChatMessage chatMessage = ChatMessage.createChatMessage(messageDTO, chatRoom, guest);
+        chatMessageRepository.save(chatMessage);
+
+        ChatRoomDTO chatRoomDTO = roomToDTO(chatRoom);
         return chatRoomDTO;
     }
 
+    // 채탕방 목록 정보
     public List<ChatRoomDTO> getAllRooms(Long userId) {
         return chatRoomRepository.findByHostUser_UserId(userId).stream()
                 .map(this::roomToDTO)
                 .collect(Collectors.toList());
     }
 
-    // 특정 채팅방 정보만. 메시지는 아직 >> 같이??
+    /*public List<ChatRoomDTO> findAllRoom() {  // 채팅방 최근 순서로 반환
+        List chatRoomDTOList = chatRoomRepository.findAll();
+        Collections.reverse(chatRoomDTOList);
+        return chatRoomDTOList;
+    }*/
+
+    // 특정 채팅방 정보만. 메시지는 아직 >> 같이?? 채팅방 위에 게시물 정보 같이 전달(채팅방 정보)
     public ChatRoomDTO selectRoom(Long roomId) {
         ChatRoom chatRoom = chatRoomRepository.findById(roomId)
                 .orElseThrow(() -> new IllegalArgumentException("No data"));
@@ -52,10 +72,10 @@ public class ChatService {
 
 
     // 메시지
-    public ChatMessageDTO createMessage(ChatMessageDTO chatMessageDTO) {
+    public ChatMessageDTO createMessage(ChatMessageDTO chatMessageDTO, Long userId) {
         ChatRoom chatRoom = chatRoomRepository.findById(chatMessageDTO.getRoomId())
                 .orElseThrow(() -> new IllegalArgumentException("No room found for the provide id"));
-        User user = userRepository.findById(chatMessageDTO.getSender())
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("No user found for the provide id"));
         ChatMessage newMessage = ChatMessage.createChatMessage(chatMessageDTO, chatRoom, user);
         chatMessageRepository.save(newMessage);
@@ -63,7 +83,7 @@ public class ChatService {
     }
 
     // 채팅방의 모든 메시지 가져오기
-    public List<ChatMessageDTO> getAllMessages(Long rommId) {
+    public List<ChatMessageDTO> getAllMessages(Long roomId) { // 쿼리 작성 필요
         return chatMessageRepository.findAll().stream()
                 .map(this::messageToDTO)
                 .collect(Collectors.toList());
@@ -74,7 +94,6 @@ public class ChatService {
     public ChatRoomDTO roomToDTO(ChatRoom room) {
         return ChatRoomDTO.builder()
                 .roomId(room.getRoomId())
-                .report(room.getReport())
                 .build();
     }
 
