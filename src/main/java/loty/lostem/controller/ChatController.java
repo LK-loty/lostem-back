50f41609214b7b0cdf14b39154b1f6cde51e53eb
kim <yahyunjin5@gmail.com>
2024-02-14 18:42:44 +0900

    edit parameter and method of chatController - get userId from token of header - contain all chat crud service not using socket

--- src/main/java/loty/lostem/controller/ChatController.java
+++ src/main/java/loty/lostem/controller/ChatController.java
@@ -3,8 +3,7 @@ package loty.lostem.controller;
 import lombok.RequiredArgsConstructor;
 import loty.lostem.dto.ChatMessageDTO;
 import loty.lostem.dto.ChatRoomDTO;
-import loty.lostem.entity.ChatMessage;
-import loty.lostem.entity.ChatRoom;
+import loty.lostem.jwt.TokenProvider;
 import loty.lostem.service.ChatService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +15,20 @@ import java.util.List;
 @RequestMapping("/api/chat")
 public class ChatController {
     private final ChatService chatService;
+    private final TokenProvider tokenProvider;
 
     // 채팅방
     @PostMapping("/room/create")
-    public ResponseEntity<ChatRoomDTO> createRoom(@RequestBody ChatRoomDTO chatRoomDTO) {
-        chatService.createRoom(chatRoomDTO);
-        return ResponseEntity.ok(chatRoomDTO);
+    public ResponseEntity<ChatRoomDTO> createRoom(@RequestBody ChatMessageDTO messageDTO, @RequestHeader("Authorization") String token) {
+        Long userId = tokenProvider.getUserId(token);
+        ChatRoomDTO dto = chatService.createRoom(messageDTO, userId);
+        return ResponseEntity.ok(dto);
     }
 
     // 채팅방 목록
-    @GetMapping("/room/read/user/{id}")
-    public ResponseEntity<List<ChatRoomDTO>> readUserRoom(@PathVariable Long userId) {
+    @GetMapping("/room/read/user")
+    public ResponseEntity<List<ChatRoomDTO>> readUserRoom(@RequestHeader("Authorization") String token) {
+        Long userId = tokenProvider.getUserId(token);
         List<ChatRoomDTO> chatRoomDTOList = chatService.getAllRooms(userId);
         return ResponseEntity.ok(chatRoomDTOList);
     }
@@ -38,4 +40,11 @@ public class ChatController {
         return ResponseEntity.ok(chatRoomDTO);
     }
 
+    // 채팅방 채팅 내역
+    @GetMapping("/chat/get")
+    public ResponseEntity<List<ChatMessageDTO>> getMessages(@PathVariable Long roomId) {
+        // 채팅방에서의 모든 메시지를 가져오는 요청을 서비스에 전달하여 처리
+        List<ChatMessageDTO> messages = chatService.getAllMessages(roomId);
+        return ResponseEntity.ok(messages);
+    }
 }
