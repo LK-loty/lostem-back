c021996f1563bda33fba0ea6e1cd65fe2308fb69
kim <yahyunjin5@gmail.com>
2024-02-25 14:40:24 +0900

    fix chatRoomDTO data - use user's tag, not userId

--- src/main/java/loty/lostem/dto/ChatRoomDTO.java
+++ src/main/java/loty/lostem/dto/ChatRoomDTO.java
@@ -9,9 +9,9 @@ import lombok.*;
 public class ChatRoomDTO {
     private Long roomId;
 
-    private Long hostUserId;  // 방 만든 사람
+    private String hostUserTag;  // 방 만든 사람 말고 게시글 주인
 
-    private String guestUserTag;  // 1:1 대화 상대
+    private String guestUserTag;  // 채팅 시도하는 사람
 
     private Long postId;
 
