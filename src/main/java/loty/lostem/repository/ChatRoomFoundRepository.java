de8470382d9ef788dbed8ea89d35bc5b0d5cb07e
kim <yahyunjin5@gmail.com>
2024-02-25 14:38:58 +0900

    add findByUserId to roomRepository - find lostChatRoom and foundChatRoom by userId

--- src/main/java/loty/lostem/repository/ChatRoomFoundRepository.java
+++ src/main/java/loty/lostem/repository/ChatRoomFoundRepository.java
@@ -3,7 +3,11 @@ package loty.lostem.repository;
 import loty.lostem.entity.FoundChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomFoundRepository extends JpaRepository<FoundChatRoom, Long> {
     //FoundChatRoom findByPostFound_IdAndHostUser_UserIdAndGuestUser_UserId(Long postFoundId, Long hostUserId, Long guestUserId);
     FoundChatRoom findByHostUser_UserIdAndGuestUser_UserId(Long hostUserId, Long guestUserId);
+
+    List<FoundChatRoom> findByGuestUser_UserId(Long userId);
 }
--- src/main/java/loty/lostem/repository/ChatRoomLostRepository.java
+++ src/main/java/loty/lostem/repository/ChatRoomLostRepository.java
@@ -3,7 +3,11 @@ package loty.lostem.repository;
 import loty.lostem.entity.LostChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomLostRepository extends JpaRepository<LostChatRoom, Long> {
     //LostChatRoom findByPostLost_IdAndHostUser_UserIdAndGuestUser_UserId(Long postLostId, Long hostUserId, Long guestUserId);
     LostChatRoom findByHostUser_UserIdAndGuestUser_UserId(Long hostUserId, Long guestUserId);
+
+    List<LostChatRoom> findByGuestUser_UserId(Long userId);
 }
