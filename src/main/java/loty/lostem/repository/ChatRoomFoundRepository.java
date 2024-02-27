f2a1eec9c6b1514dfb37189ba9316451c556744e
kim <yahyunjin5@gmail.com>
2024-02-27 15:15:44 +0900

    fix chatRoomRepositories - because of change to Composition, change query method to use userId

--- src/main/java/loty/lostem/repository/ChatRoomFoundRepository.java
+++ src/main/java/loty/lostem/repository/ChatRoomFoundRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomFoundRepository extends JpaRepository<FoundChatRoom, Long> {
     //FoundChatRoom findByPostFound_IdAndHostUser_UserIdAndGuestUser_UserId(Long postFoundId, Long hostUserId, Long guestUserId);
-    FoundChatRoom findByHostUser_UserIdAndGuestUser_UserId(Long hostUserId, Long guestUserId);
+    FoundChatRoom findByChatRoom_HostUser_UserIdAndChatRoom_GuestUser_UserId(Long hostUserId, Long guestUserId);
 }
--- src/main/java/loty/lostem/repository/ChatRoomLostRepository.java
+++ src/main/java/loty/lostem/repository/ChatRoomLostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomLostRepository extends JpaRepository<LostChatRoom, Long> {
     //LostChatRoom findByPostLost_IdAndHostUser_UserIdAndGuestUser_UserId(Long postLostId, Long hostUserId, Long guestUserId);
-    LostChatRoom findByHostUser_UserIdAndGuestUser_UserId(Long hostUserId, Long guestUserId);
+    LostChatRoom findByChatRoom_HostUser_UserIdAndChatRoom_GuestUser_UserId(Long hostUserId, Long guestUserId);
 }
