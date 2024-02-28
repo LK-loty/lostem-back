91a6250bb37cc240c2e7a6f921c16bd2f2cae4c3
kim <yahyunjin5@gmail.com>
2024-02-29 00:14:31 +0900

    update chatRoomListDTO and ChatUserDTO - add user profile, nickname, tag. need to check and use counterpart's data

--- src/main/java/loty/lostem/dto/ChatUserDTO.java
+++ src/main/java/loty/lostem/dto/ChatUserDTO.java
@@ -2,17 +2,12 @@ package loty.lostem.dto;
 
 import lombok.*;
 
-import java.util.List;
-
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Getter
-public class ChatRoomListDTO {
-    private List<ChatRoomDTO> chatRoomLostList;
-    private List<ChatRoomDTO> chatRoomFoundList;
+public class ChatUserDTO {
     private String profile;
     private String nickname;
     private String tag;
-    private ChatRoomDTO chatRoomDTO;
 }
