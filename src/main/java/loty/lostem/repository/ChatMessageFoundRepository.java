8bb7360329979cad317216f28f887e0190a0e63d
kim <yahyunjin5@gmail.com>
2024-03-01 17:47:55 +0900

    add get message and return to DTO at ChatService - add messageToDTO to ChatService - add query method find by roomId to ChatMessageRepository (Lost, Found) - getAll Messages return ChatMessageDTO List of roomId - use this service to ChatController

--- src/main/java/loty/lostem/repository/ChatMessageFoundRepository.java
+++ src/main/java/loty/lostem/repository/ChatMessageFoundRepository.java
@@ -3,5 +3,8 @@ package loty.lostem.repository;
 import loty.lostem.entity.ChatMessageFound;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageFoundRepository extends JpaRepository<ChatMessageFound, Long> {
+    List<ChatMessageFound> findByFoundChatRoom_RoomId(Long roomId);
 }
--- src/main/java/loty/lostem/repository/ChatMessageLostRepository.java
+++ src/main/java/loty/lostem/repository/ChatMessageLostRepository.java
@@ -3,5 +3,8 @@ package loty.lostem.repository;
 import loty.lostem.entity.ChatMessageLost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageLostRepository extends JpaRepository<ChatMessageLost, Long> {
+    List<ChatMessageLost> findByLostChatRoom_RoomId(Long roomId);
 }
