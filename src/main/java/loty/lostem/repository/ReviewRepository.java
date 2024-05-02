f19d0211c7c94714d09d2229c23dd3054b0581c0
kim <yahyunjin5@gmail.com>
2024-05-02 18:37:15 +0900

    fix read Review - return list

--- src/main/java/loty/lostem/repository/ReviewRepository.java
+++ src/main/java/loty/lostem/repository/ReviewRepository.java
@@ -3,8 +3,9 @@ package loty.lostem.repository;
 import loty.lostem.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    Optional<Review> findByUser_Tag(String tag);
+    List<Review> findByUser_Tag(String tag);
 }
